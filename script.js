db016e7b5ff8f07e973e66f511cc1db3ef4f7bf1
Ivan Goncharuk <goncharukivan.biz@gmail.com>
2024-10-14 15:46:40 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll('.section-2');
+const options = {
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add('appear');
+        observer.unobserve(entry.target);
+    });
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
